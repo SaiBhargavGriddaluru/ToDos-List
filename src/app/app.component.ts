97cac60136e04c7a221fee46325e416a1ea33963
SaiBhargavGridddaluru <griddalurusaibhargav@gmail.com>
2024-10-24 17:13:20 +0530

    Updated Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ListToDosComponent } from './list-to-dos/list-to-dos.component';
+import { MenuComponent } from "./menu/menu.component";
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, WelcomeComponent, ListToDosComponent],
+  imports: [RouterOutlet, LoginComponent, WelcomeComponent, ListToDosComponent, MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
